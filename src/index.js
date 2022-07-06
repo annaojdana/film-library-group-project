4be64033ec5387d4215e3921891db10565675c18
dawidmarek95x <dawid.marek95x@gmail.com>
2022-07-06 23:03:55 +0200

    feature/save-and-load-genres
    Dodane funkcje:
    
    1. fetchGenresNames() - funkcja do pobierania danych z backendu z nazwami wszystkich rodzajów filmów
    
    2. memorizeGenres() - funkcja zapisująca w localStorage klucz z tablicą z obiektami wszystkich genres pobranych z backendu
    
    3. getFromLocalStorage(key) - funkcja uniwersalna do pobierania danych z localStorage. Wystarczy  zaimportować komponent do innego i wywołać wraz nazwą klucza, który chcemy pobrać
    
    4. getGenresNames(genreIdsOfMovie) - funkcja przyjmująca argument w postaci tablicy i zwracająca nazwy genres dla wybranego elementu.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './components/header/header';
 import './components/popularMovies/popularMovies'
 import './components/footer/footer.js';
 
-
+import memorizeGenres from './components/memorizeGenres/memorizeGenres';
