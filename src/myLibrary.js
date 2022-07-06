cef8697fd0f71cf66a22efb400f30f6314890248
Anna Ojdana <anna.ojdana@gmail.com>
2022-07-06 19:56:19 +0200

    bugFix - container

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,4 +1,6 @@
 import './main.scss';
+
 import './components/headerLibrary/headerLibrary';
 import './components/header/header';
-import './components/footer/footer.js';
+import './components/libraryMovies/libraryMovies'
+import './components/footer/footer';
