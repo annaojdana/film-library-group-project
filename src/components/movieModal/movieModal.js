d265adfbe42684431d6d801bb61496b35c679282
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-07-08 01:27:29 +0200

    Modal toggle fix
    Uruchomiono modal
    Dodano funkcję inicjalizującą listener dla listy filmów do wyświetlania modali
    Przekształcono funkcję toggleModal() na wewnętrzną dla pliku - nie jest potrzebna zewnętrznie

--- src/components/movieModal/movieModal.js
+++ src/components/movieModal/movieModal.js
@@ -1,16 +1,26 @@
 import './movieModal.scss';
 
-
-
 const closeModalBtn = document.querySelector('[data-modal-close]');
 const modal = document.querySelector('[data-modal]');
+console.log(modal);
 
 console.log(closeModalBtn);
 
+closeModalBtn.addEventListener('click', toggleModal);
 
- closeModalBtn.addEventListener("click", toggleModal);
-
-export function toggleModal() {
+function toggleModal() {
   modal.classList.toggle('is-hidden');
-    }
+}
 
+export function initializeModal() {
+  const timer = setInterval(function checkForMarkup() {
+    cards = document.querySelectorAll('[data-modal-open]');
+
+    if (cards.length !== 0) {
+      for (const card of cards) {
+        card.addEventListener('click', toggleModal);
+      }
+      clearInterval(timer);
+    }
+  }, 10);
+}
--- src/components/popularMovies/popularMovies.js
+++ src/components/popularMovies/popularMovies.js
@@ -7,8 +7,26 @@ import moviesListMarkup from '../moviesListMarkup/moviesListMarkup.js';
 // Wywołanie funkcji - markup danych dla 'trending
 moviesListMarkup();
 
-import { toggleModal } from '../movieModal/movieModal';
+import { toggleModal, initializeModal } from '../movieModal/movieModal';
 
-const openModalBtn = document.querySelector('[data-modal-open]');
+// Funkcja dla inicjalizacji modala dla listy filmów,
+// funkcja dodajelistener dla każdej wyświetlonej karty.
+// Ze względu, iż fetch jest asynchroniczny,
+// obecność kart w HTMLu jest sprawdzana co 10ms
 
-openModalBtn.addEventListener("click", toggleModal);
+// function initializeModal() {
+//   const timer = setInterval(function checkForMarkup() {
+//     cards = document.querySelectorAll('[data-modal-open]');
+
+//     if (cards.length !== 0) {
+//       for (const card of cards) {
+//         card.addEventListener('click', toggleModal());
+//         clearInterval(timer);
+//       }
+//     } else {
+//       console.log('nope man, again');
+//     }
+//   }, 10);
+// }
+
+initializeModal();
