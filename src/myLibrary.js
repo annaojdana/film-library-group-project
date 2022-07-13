60ba270554292fc0922fb85c6b69c62fd83cffec
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-07-13 12:08:05 +0200

    Some touches
    - Wykonano ostylowanie dla aktywnych przycisków w headerze strony My library
    
    -  Odrobinę zrefaktoryzowano komponent paginacji
    
    - Poprawnie rozwiązano konflikty poprzednich zmergowanych gałęzi

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -5,34 +5,3 @@ import './main.scss';
 import './components/headerLibrary/headerLibrary';
 import './components/loadMoviesList/loadMoviesList';
 import './components/footer/footer';
-
-// Rendering popularnych filmów na stronę główną i wywołanie funkcji dla 'trending'
-import moviesListMarkup from './components/moviesListMarkup/moviesListMarkup';
-moviesListMarkup('queue');
-
-// Selektory przycisków "watched" i "queue"
-const selectorButtons = document.querySelectorAll('[data-display-selector]');
-console.log(selectorButtons);
-
-function myLibraryDisplaySelectorHandler(event) {
-  switch (event.target.dataset.displaySelector) {
-    case 'watched':
-      console.log('Displaying watched...');
-      moviesListMarkup('watched');
-      break;
-
-    case 'queue':
-      console.log('Displaying queue...');
-      moviesListMarkup('queue');
-
-      break;
-
-    default:
-      console.log('invalid target');
-      break;
-  }
-}
-
-for (const button of selectorButtons) {
-  button.addEventListener('click', myLibraryDisplaySelectorHandler);
-}
