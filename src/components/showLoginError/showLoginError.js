2eca99b96ca1c677ec19061f7cba86321ba289f0
dawidmarek95x <dawid.marek95x@gmail.com>
2022-07-15 17:39:01 +0200

    feature/code-review-for-auth

--- src/components/showLoginError/showLoginError.js
+++
@@ -1,9 +0,0 @@
-import { AuthErrorCodes } from "firebase/auth";
-
-export default function showLoginError(error) {
-  if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
-    loginErrorMessage.innerHTML = "Wrong password! Try again.";   // (loginErrorMessage) - Here we need use name of constant for div output to show message obtained by querySelector
-  } else {
-    loginErrorMessage.innerHTML = `Error: ${error.message}`;      // (loginErrorMessage) - Here we need use name of constant for div output to show message obtained by querySelector
-  }
-}
