36f603a25b7145e0236f4b7a595b50eda3f73924
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-07-18 19:32:35 +0200

    bugfix

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -2,9 +2,9 @@
 import './main.scss';
 // Obsługa spinnera
 import './components/loader/loader';
-spinner();
 import './components/headerLibrary/headerLibrary';
-import './components/loadMoviesList/loadMoviesList';import { Loading } from 'notiflix';
+import './components/loadMoviesList/loadMoviesList';
+import { Loading } from 'notiflix';
 import spinner from './components/loader/loader';
 
-
+spinner();
