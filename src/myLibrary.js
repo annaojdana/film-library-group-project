dfda4112534af3a84e73387c6e70263860579462
Marek Kalus <marektg2@gmail.com>
2022-07-18 10:09:59 +0200

    update

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -2,8 +2,9 @@
 import './main.scss';
 // Obsługa spinnera
 import './components/loader/loader';
-loader();
+spinner();
 import './components/headerLibrary/headerLibrary';
 import './components/loadMoviesList/loadMoviesList';import { Loading } from 'notiflix';
+import spinner from './components/loader/loader';
 
 
