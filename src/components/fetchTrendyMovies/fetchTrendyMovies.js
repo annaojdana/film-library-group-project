1aab12d1a2f5965dfe3c936f701050c7e0264b48
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-07-21 16:24:35 +0200

    modal scrollbar bugfix
    usunięto wyświetlanie scrollbara w modalu

--- src/components/fetchTrendyMovies/fetchTrendyMovies.js
+++ src/components/fetchTrendyMovies/fetchTrendyMovies.js
@@ -1,25 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
+const language = 'pl-PL'
 
-export default async function fetchTrendyMovies(pageNumber = 1, period = "week",) {
+export default async function fetchTrendyMovies(
+  pageNumber = 1,
+  period = 'week'
+) {
   const endPoints = {
-    media_type: "movie",
+    media_type: 'movie',
     time_window: period,
-  }
+  };
 
   const params = new URLSearchParams({
-    api_key: "2f202abcab3fe0934220a17698275697",
+    api_key: '2f202abcab3fe0934220a17698275697',
     page: pageNumber,
+
   });
 
   try {
-    const {media_type, time_window} = endPoints;
+    const { media_type, time_window } = endPoints;
     const response = await axios.get(`
     https://api.themoviedb.org/3/trending/${media_type}/${time_window}?${params}`);
     return response.data;
   } catch (error) {
     console.log(`${error.name}: ${error.message}`);
-  };
-};
+  }
+}
 
 /* Wywoływanie funkcji:
 fetchTrendyMovies(); - wywołanie domyślne tj. page = 1 i wyszukiwanie popularnych z tygodnia
