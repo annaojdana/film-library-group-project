4492571e5f9b9b515ad7765bca7bc7a3a06805cb
Anna Ojdana <anna.ojdana@gmail.com>
2022-07-22 15:47:26 +0200

    checked icon

--- src/components/myAccount/myAccount.js
+++ src/components/myAccount/myAccount.js
@@ -1,2 +1,56 @@
 import './myAccount.scss'
+import './../../scss/_backdrop.scss'
+import {
+  disableScrolling,
+  enableScrolling,
+} from '../scrollToggle/scrollToggle';
 
+
+const generalIcon = document.querySelector(".general"),
+  passwordIcon = document.querySelector(".password"),
+  avatarIcon = document.querySelector(".avatar"),
+  deleteIcon = document.querySelector(".delete-account");
+
+
+const closeModalBtn = document.querySelector('[data-account-close]');
+const modalAccount = document.querySelector('[data-account]');
+const backdrop = document.querySelector('body');
+
+const links = [...document.querySelectorAll('.account__link')];
+const setActive = el => {
+  [...el.parentElement.children].forEach(sib =>
+  sib.children[0].classList.remove('checked')
+  );
+el.children[0].classList.add('checked');
+}
+links.forEach(el => el.addEventListener('click', e => setActive(el)));
+
+const closeModal = () => {
+  console.log("object");
+  modalAccount.classList.add('is-hidden');
+  backdrop.removeEventListener('keydown', closeEscKey);
+  enableScrolling();
+}
+
+const openModal = () => {
+  modalAccount.classList.remove('is-hidden');
+  backdrop.addEventListener('keydown', closeEscKey);
+  disableScrolling();
+}
+const closeEscKey = (e) => {
+  let keyCode = e.keyCode;
+  if (keyCode === 27) {
+    //keycode is an Integer, not a String
+    closeModal();
+  }
+}
+const changeSide = (icon) => {
+  icon.path[1].classList.toggle('checked');
+  // icon.classList.remove('is-hidden');
+  // backdrop.addEventListener('keydown', closeEscKey);
+  // disableScrolling();
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+// modalAccount.addEventListener('click', closeByClick);
+// accountLink.addEventListener('click', openModal);
--- src/components/teamModal/teamModal.js
+++ src/components/teamModal/teamModal.js
@@ -9,25 +9,18 @@ const teamLink = document.querySelector('[data-team]');
 const closeModalBtn = document.querySelector('[data-team-modal-close]');
 const modalTeam = document.querySelector('[data-team-modal]');
 const backdrop = document.querySelector('body');
-
-closeModalBtn.addEventListener('click', closeModal);
-modalTeam.addEventListener('click', closeByClick);
-teamLink.addEventListener('click', openModal);
-
-modalTeam.classList.remove('is-hidden-bugfix'); //Bugfix
-
-function closeModal() {
+const closeModal = () => {
   modalTeam.classList.add('is-hidden');
   backdrop.removeEventListener('keydown', closeEscKey);
   enableScrolling();
 }
 
-function openModal() {
+const openModal = () => {
   modalTeam.classList.remove('is-hidden');
   backdrop.addEventListener('keydown', closeEscKey);
   disableScrolling();
 }
-function closeEscKey(e) {
+const closeEscKey = (e) => {
   let keyCode = e.keyCode;
   if (keyCode === 27) {
     //keycode is an Integer, not a String
@@ -35,8 +28,14 @@ function closeEscKey(e) {
   }
 }
 
-function closeByClick(e) {
+const closeByClick = () => {
   if (e.target === modalTeam) {
     closeModal();
   }
 }
+closeModalBtn.addEventListener('click', closeModal);
+modalTeam.addEventListener('click', closeByClick);
+teamLink.addEventListener('click', openModal);
+
+modalTeam.classList.remove('is-hidden-bugfix'); //Bugfix
+
