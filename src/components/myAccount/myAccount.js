1ca4612d748b43f14317b91e063122ff6c6b6a4f
dawidmarek95x <dawid.marek95x@gmail.com>
2022-07-23 22:56:52 +0200

    account modal connected with opening btn

--- src/components/myAccount/myAccount.js
+++ src/components/myAccount/myAccount.js
@@ -4,12 +4,12 @@ import {
   enableScrolling,
 } from '../scrollToggle/scrollToggle';
 
+const openAccountBtn = document.querySelector('[data-account-open]');
 const closeAccountBtn = document.querySelector('[data-account-close]');
 const modalAccount = document.querySelector('[data-account]');
 const backdrop = document.querySelector('body');
 const avatarUplaoder = document.querySelector('.account__uploader');
 const avatarImage = document.querySelector('.account__img');
-
 const links = [...document.querySelectorAll('.account__link')];
 const cards = [...document.querySelectorAll('.account__card')];
 
@@ -19,6 +19,7 @@ const checkActiveIcon = el => {
   accountLinks.forEach(sib => sib.children[0].classList.remove('checked'));
   currentIcon.classList.add('checked');
 };
+
 const setCard = el => {
   const cardId = el.href.split('#')[1];
   cards.forEach(sib => {
@@ -27,23 +28,24 @@ const setCard = el => {
       : sib.classList.add('active-card');
   });
 };
+
 const accountSupport = el => {
   checkActiveIcon(el);
   setCard(el);
 };
 
 const closeModal = () => {
-  console.log('object');
   modalAccount.classList.add('is-hidden-account');
   backdrop.removeEventListener('keydown', closeEscKey);
   enableScrolling();
 };
 
 const openModal = () => {
-  modalAccount.classList.remove('is-hidden');
+  modalAccount.classList.remove('is-hidden-account');
   backdrop.addEventListener('keydown', closeEscKey);
   disableScrolling();
 };
+
 const closeEscKey = e => {
   let keyCode = e.keyCode;
   if (keyCode === 27) {
@@ -56,6 +58,7 @@ const closeByClick = e => {
     closeModal();
   }
 };
+
 const setAvatar = input => {
   let file = input.target.files[0];
   let reader = new FileReader();
@@ -66,6 +69,8 @@ const setAvatar = input => {
   };
   reader.readAsDataURL(file);
 };
+
+openAccountBtn.addEventListener('click', openModal);
 closeAccountBtn.addEventListener('click', closeModal);
 modalAccount.addEventListener('click', closeByClick);
 avatarUplaoder.addEventListener('change', setAvatar);
